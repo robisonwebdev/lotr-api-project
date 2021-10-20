cd3f2dac57f57698dd67f6bfd9c99608d95db2e2
robisonwebdev <david@robisonweb.dev>
2021-10-20 06:59:43 -0600

    Imported Header, Main, and Footer components. Called Header, Main, and Footer components in return div.

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import React from 'react'
+import Header from './components/Header';
+import Main from './components/Main';
+import Footer from './components/Footer';
 import './styles/App.css';
 
 const App = () => {
   return (
-    <div>
-      App
+    <div className='app'>
+      <Header />
+      <Main />
+      <Footer />
     </div>
   );
 }
