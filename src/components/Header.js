fbd3819fc2723a6c9f8a4161eda14a9883b63837
robisonwebdev <david@robisonweb.dev>
2021-10-20 07:10:28 -0600

    Imported logo from images folder. Set logo into img src. Set alt to The Lord of the Rings text logo.

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,11 @@
 import React from 'react'
 import '../styles/Header.css';
+import logo from '../images/lotr_logo.png';
 
 const Header = () => {
     return (
         <header>
-            Header
+            <img src={logo} alt='The Lord of the Rings text logo' />
         </header>
     );
 }
