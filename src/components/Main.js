60f1aaff1a30d5e5d02a881d67fde46a939a3de2
robisonwebdev <david@robisonweb.dev>
2021-10-20 08:41:30 -0600

    Imported useEffect, useState, and axios. Setup useState for items, and set to initial value of empty array. Setup useEffect. Added headers for authorization. Added fetchItems variable to get character data.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,7 +1,27 @@
-import React from 'react'
+import React, { useEffect, useState } from 'react'
+import axios from 'axios';
 import '../styles/Main.css';
 
 const Main = () => {
+    const [items, setItems] = useState([]);
+
+    useEffect(() => {
+        const headers = {
+            'Accept': 'application/json',
+            'Authorization': 'Bearer zOjjtnXi3W7jeEjqZlm8'
+        };
+
+        const fetchItems = async () => {
+            const getCharacters = await axios(`https://the-one-api.dev/v2/character`, {
+                headers: headers
+            });
+
+            console.log(getCharacters.data);
+        };
+
+        fetchItems();
+    }, [])
+
     return (
         <main>
             
