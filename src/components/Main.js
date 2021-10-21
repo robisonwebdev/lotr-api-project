9d506d1a86da87dbc498a76b41f2c3004aecaf7a
robisonwebdev <david@robisonweb.dev>
2021-10-21 10:44:44 -0600

    Imported Quotes. Changed items useState to quotes useState. Setup useEffect to get quotes and save it to quotes state. Called quote component in main.

--- src/components/Main.js
+++ src/components/Main.js
@@ -1,9 +1,10 @@
 import React, { useEffect, useState } from 'react'
 import axios from 'axios';
+import Quote from './Quote';
 import '../styles/Main.css';
 
 const Main = () => {
-    const [items, setItems] = useState([]);
+    const [quotes, setQuotes] = useState([]);
 
     useEffect(() => {
         const headers = {
@@ -11,20 +12,14 @@ const Main = () => {
             'Authorization': 'Bearer zOjjtnXi3W7jeEjqZlm8'
         };
 
-        const fetchItems = async () => {
-            const getCharacters = await axios(`https://the-one-api.dev/v2/character`, {
-                headers: headers
-            });
-
-            console.log(getCharacters.data);
-        };
-
-        fetchItems();
+        axios.get(`https://the-one-api.dev/v2/quote`, {headers})
+        .then(res => setQuotes(res.data))
+        .catch(err => console.log('Error:', err))
     }, [])
 
     return (
         <main>
-            
+            <Quote />
         </main>
     )
 };
