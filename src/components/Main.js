f5eb551019fbfdb86ddf51a0f011666ec9f99038
robisonwebdev <david@robisonweb.dev>
2021-10-23 12:58:16 -0600

    Remove characterData useState. Added quote useState. Remove testAPI function. Setup getRandomQuote function, that gets random quote and sets it to quote state. Setup useEffect to check to loading changes, if found loading is false, run getRandomQuote. Setup ternary in main, to check to loading true or false. If true send message of loading. If false send quote.

--- src/components/Main.js
+++ src/components/Main.js
@@ -4,114 +4,44 @@ import Quote from './Quote';
 import '../styles/Main.css';
 
 const Main = () => {
-    const [charactersData, setCharactersData] = useState([]);
     const [loading, setLoading] = useState(true);
-    const [quotesData, setQuotesData] = useState([]);    
+    const [quote, setQuote] = useState('');
+    const [quotesData, setQuotesData] = useState([]);
+
 
     useEffect(() => {
         const headers = {
             'Accept': 'application/json',
             'Authorization': 'Bearer zOjjtnXi3W7jeEjqZlm8'
-        }
+        };
 
         setLoading(true);
+        console.log('Getting API Data');
 
         axios.get(`https://the-one-api.dev/v2/quote`, {headers})
-             .then(res => setQuotesData(res.data))
-             .catch(err => console.log(err))
-
-        axios.get(`https://the-one-api.dev/v2/character`, {headers})
              .then(res => {
-                 setCharactersData(res.data);
-                 setLoading(false);
+                setQuotesData(res.data);
+                setLoading(false);
              })
              .catch(err => console.log(err))
-    }, [])
+    }, []);
 
-    const testAPI = () => {
+    useEffect(() => {
         if (!loading) {
-            console.log('Quotes', quotesData);
-            console.log('Characters', charactersData);
+            getRandomQuote();
         }
+    }, [loading])
+
+    const getRandomQuote = () => {
+        let randomQuote = quotesData.docs[Math.floor(Math.random() * quotesData.docs.length)]['dialog'];
+        setQuote(randomQuote);
     }
 
     return (
         <main>
-            {testAPI()}
-            <Quote />
+            {loading ? <Quote message='Loading...' /> : <Quote message={quote} />}
         </main>
     );
 }
-
-// const Main = () => {
-//     const [character, setCharacter] = useState([]);
-//     const [characterID, setCharacterID] = useState('');
-//     const [characterName, setCharacterName] = useState('');
-//     const [loading, setLoading] = useState(true);
-//     const [quotes, setQuotes] = useState([]);
-//     const [quote, setQuote] = useState('');
-//     const [headers] = useState({
-//         'Accept': 'application/json',
-//         'Authorization': 'Bearer zOjjtnXi3W7jeEjqZlm8'
-//     });
-
-//     const generateNewQuote = useCallback(
-//         () => {
-//             const randomQuote = quotes.docs[Math.floor(Math.random() * quotes.docs.length)];
-//             const newQuote = randomQuote.dialog;
-//             const characterID = randomQuote.character;
-
-//             console.log(randomQuote);
-//             setCharacterID(characterID);
-//             setQuote(newQuote);
-//         },
-//         [quotes.docs],
-//     );
-
-//     const getCharacterName = () => {
-//         const name = characterID.docs[0]['name'];
-
-//         setCharacterName(name);
-//     }
-
-//     // Initial API Load
-//     useEffect(() => {
-//         setLoading(true);
-
-//         axios.get(`https://the-one-api.dev/v2/quote`, {headers})
-//         .then(res => {
-//             setQuotes(res.data);
-//             setLoading(false);
-//         })
-//         .catch(err => console.log('Error:', err))
-//     }, [headers])    
-
-//     // Get Character Information
-//     useEffect(() => {
-//         axios.get(`https://the-one-api.dev/v2/character/${characterID}`, {headers})
-//         .then(res => {
-//             setCharacter(res.data);
-//             // getCharacterName();
-//         })
-//         .catch(err => console.log('Error:', err))
-//     }, [characterID, headers])
-
-//     // Initial Page Load
-//     useEffect(() => {
-//         if (!loading) {
-//             generateNewQuote();
-//         }
-//     }, [loading, generateNewQuote])
-
-//     return (
-//         <main>
-//             <Quote
-//                 characterName={characterName}
-//                 generateNewQuote={generateNewQuote}
-//                 quote={quote}
-//             />
-//         </main>
-//     )
-// };
 
 export default Main
