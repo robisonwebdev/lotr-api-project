e6eec3ca2b74afa611d85fab5f1dbcf7cd8ece5d
robisonwebdev <david@robisonweb.dev>
2021-10-25 04:52:06 -0600

    Changed logo

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import '../styles/Header.css';
-import logo from '../images/lotr_logo.png';
+import logo from '../images/lotr_logo2.png';
 
 const Header = () => {
     return (
